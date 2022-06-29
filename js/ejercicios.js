89eb68e01ba06344b530d9224a2952051ef5e259
Javier Uncos <javiersrfc30@gmail.com>
2022-06-29 20:02:16 -0300

    comenzamos a desarrollar los ejercicios de javascript

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -11,3 +11,21 @@
 // let saludo = "wordl";
 // document.write("<h1>Hello " + (saludo) + "</h1>");
 
+
+
+// ejercicio nuemro 3 
+// escribir un programa que escriba enpantalla el resultado de 3+5
+// let numero = 2;
+// document.write("<p>Resultado: " + (3 + numero) + "</p>")
+
+
+
+// ejercicio numero 4 
+// codigo de dos lineas que pida nombre de usuario con prompt
+// let persona = prompt("ingresa un nombre");
+// document.write("<p>hola "+(persona) + "</p>");
+
+
+let numero = prompt();
+let numeroDos = prompt();
+document.write("<p>resultado: "+ (numero+numeroDos) + "</p>");
