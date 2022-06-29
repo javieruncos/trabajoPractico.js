c29bbfc85961a934cef5f0ec3c46b664c9301d52
Javier Uncos <javiersrfc30@gmail.com>
2022-06-29 19:10:46 -0300

    iniciamos con los ejercicios del javascript

---
+++ js/ejercicios.js
@@ -0,0 +1,13 @@
+
+// ejercicio numero 1
+// mostramos un mensaje en la pagina 
+// let mensaje = "hola esta es una practica de js";
+// alert(mensaje);
+
+
+
+// ejercico numero 2
+// mostramos un mensaje en pantalla
+// let saludo = "wordl";
+// document.write("<h1>Hello " + (saludo) + "</h1>");
+
