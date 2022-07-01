675172374a55f1bf176b372c10a7f3d929c870a1
Javier Uncos <javiersrfc30@gmail.com>
2022-07-01 12:11:53 -0300

    realizamos el ejercicio 7 del campus

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -28,6 +28,43 @@
 
 // ejercicio numero 5 solicitar al usuario 2 numeros sumarlos y mostrar el resultado en pantalla
 
-let numero = parseInt(prompt("ingresa el primer numero"));
-let numero2 = parseInt(prompt("ingresa el segundo numero"));
-document.write("<p>Resultado: " + (numero+numero2) + "</p>");
+// let numero = parseInt(prompt("ingresa el primer numero"));
+// let numero2 = parseInt(prompt("ingresa el segundo numero"));
+// document.write("<p>Resultado: " + (numero+numero2) + "</p>");
+
+// ejercicio numero 6 pedir al usuario dos numeros y mostrar en pantalla cual es el mayor
+
+// let numero = parseInt(prompt("ingresa un numero"));
+// let numero2 = parseInt(prompt("ingresa el segundo numero"))
+// if (numero > numero2){
+//     document.write(numero);
+// }else if(numero<numero2) {
+//     document.write(numero2);
+// } 
+// else{
+//     document.write(numero);
+// }
+
+// ejercicio 7 pedir al usuario 3 numeros y mostrar en pantalla el numero mayor
+// let numero = parseInt(prompt("ingresa un numero"));
+// let numero2 = parseInt(prompt("ingresa el segundo un numero"));
+// let numero3 = parseInt(prompt("ingresa el tercer  numero"));
+
+// if (numero >= numero2 && numero >= numero3){
+//     document.write(numero);
+// }
+
+// else if (numero2 >= numero && numero2 >= numero3){
+//     document.write(numero2);
+// }
+
+// else if (numero3 >= numero && numero3 >= numero2){
+//     document.write(numero3); 
+// }
+
+
+
+
+   
+
+ 
