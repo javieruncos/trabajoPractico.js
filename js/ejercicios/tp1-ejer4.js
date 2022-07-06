c69a33e4f468e261a44a223bc13616be0e3d492f
Javier Uncos <javiersrfc30@gmail.com>
2022-07-06 12:40:54 -0300

    actualizacion de los ejercicios  4 , 5 y 6

--- js/ejercicios/tp1-ejer4.js
+++ js/ejercicios/tp1-ejer4.js
@@ -2,4 +2,4 @@
 // ejercicio numero 4 
 // codigo de dos lineas que pida nombre de usuario con prompt
 let persona = prompt("ingresa un nombre");
-document.write("<p>hola "+(persona) + "</p>");
+document.write("Hola " + (persona));
--- js/ejercicios/tp1-ejer6.js
+++ js/ejercicios/tp1-ejer6.js
@@ -1,15 +1,5 @@
 // ejercicio numero 6 pedir al usuario dos numeros y mostrar en pantalla cual es el mayor
 
-// let numero = parseInt(prompt("ingresa un numero"));
-// let numero2 = parseInt(prompt("ingresa el segundo numero"))
-// if (numero > numero2){
-//     document.write(numero);
-// }else if(numero<numero2) {
-//     document.write(numero2);
-// } 
-// else{
-//     document.write(numero);
-// }
 
 let numero1 = parseInt(prompt("ingresa un numero"));
 let numero2 = parseInt(prompt("ingresa el segundo numero"));
@@ -18,5 +8,5 @@ if (numero1 > numero2){
 }else if (numero2 == numero1){
     document.write("los numeros ingresados son iguales: " + numero2)
 }else{
-    document.write("el segundo numero es el mayor" )
+    document.write("el segundo numero es el mayor: " + (numero2) )
 }
