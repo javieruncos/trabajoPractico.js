cde7d25495cb1f8ef6ad34c48d138367b1b084b3
Javier Uncos <javiersrfc30@gmail.com>
2022-07-06 20:30:54 -0300

    resolvimos el ejercicio 9 y terminamos el practico 1

--- js/ejercicios/tp1-ejer10.js
+++ js/ejercicios/tp1-ejer10.js
@@ -1,13 +1,13 @@
 let numero = parseInt(prompt("ingresa un numero"));
 
 if(numero % 2 === 0){
-    document.write(true); 
+    document.write("el numero es divisible por: 2"); 
 }else if(numero % 3 === 0 ){
-    document.write(true);
+    document.write("el numero es divisible  por : 3");
 }else if (numero % 5 === 0){
-    document.write(true);
+    document.write("el numero es divisible por : 5");
 }else if(numero % 7 === 0){
-    document.write(true);
+    document.write("el numero es divisible por 7");
 }else{
-    document.write(false);
+    document.write("el numero no es divisible por los anteriores");
 }
--- js/ejercicios/tp1-ejer11.js
+++ js/ejercicios/tp1-ejer11.js
@@ -1,30 +1,50 @@
+
+// eh creado dos ejemplos en el primero mostramos los cuatro valores e indicamos por cual es divisible y por cuales no , en el segundo ejemplo solo mostramos por cuale es divisible
+
 let numero = parseInt(prompt("ingresa un numero"));
 
 if(numero  % 2 === 0){
-  document.write("el numero es divisible por 2" + "<br>")
+  document.write("el numero es divisible por: 2" + "<br>")
 }else{
-  document.write(false  + "<br>")
+  document.write("el numero no es divisible por: 2  "  + "<br>")
 }
 
 if(numero % 3 === 0){
-  document.write("el numero es divisible por 3" + "<br>")
+  document.write("el numero es divisible por: 3" + "<br>")
 }else{
-  document.write(false + "<br>")
+  document.write("el numero no es divisible por: 3  "  + "<br>")
 }
 
 if(numero % 5 === 0){
-  document.write("el numero es divisible por 5" + "<br>")
+  document.write("el numero es divisible por: 5" + "<br>")
 }else{
-  document.write(false  + "<br>")
+  document.write("el numero no es divisible por: 5 "   + "<br>")
 }
 
 if(numero % 7 === 0){
-  document.write("el numero es divisible por 7" + "<br>")
+  document.write("el numero es divisible por: 7" + "<br>")
 }else{
-  document.write(false  + "<br>")
+  document.write("el numero no es divisible por: 7 "  + "<br>")
 }
 
 
 
+// // ejemplo 2
 
+// let numero = parseInt(prompt("ingresa un numero"));
 
+// if(numero  % 2 === 0){
+//   document.write("el numero es divisible por: 2" + "<br>")
+// }
+
+// if(numero % 3 === 0){
+//   document.write("el numero es divisible por: 3" + "<br>")
+// }
+
+// if(numero % 5 === 0){
+//   document.write("el numero es divisible por: 5" + "<br>")
+// }
+
+// if(numero % 7 === 0){
+//   document.write("el numero es divisible por 7" + "<br>")
+// }
